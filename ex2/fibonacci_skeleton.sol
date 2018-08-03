f44fd8f3e1f0e33fb4abe5e69e7bb38b094e51b3
mahdida <42056652+mahdida@users.noreply.github.com>
2018-08-03 11:49:49 +0430

    Update fibonacci_skeleton.sol

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -2,10 +2,35 @@ pragma solidity ^0.4.16;
 
 
 contract Fibonacci {
+    
+    uint result;
+    mapping(uint => uint) FibonacciList;
+    event FibonacciMade(uint p , uint r);
+    
 	function calculate(uint position) returns (uint result) {
 		/* Carry out calculations to find the nth Fibonacci number */
+		require(position>=0);
+		
+		if(position == 0){
+		    return 0;
+		}
+		else if(position == 1){
+		    return 1;
+		}
+		else{
+		    if(FibonacciList[position] !=0){
+		        return FibonacciList[position];
+		    }
+		}
+		result = calculate(position -1) + calculate(position -2);
+		FibonacciList[position] = result;
+		FibonacciMade(position , result);
+		return result;
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function(){
+	    
+	}
 
 }
